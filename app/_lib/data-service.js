7b4ab9124c67f9953b3859a5ac9b892365cde963
GhadeerAlbattarni <ghadeeralbattarni@gmail.com>
2024-11-14 20:45:36 -0500

    Add nested routes - pages - layout to Account page

---
+++ app/_lib/data-service.js
@@ -0,0 +1,16 @@
+import { eachDayOfInterval } from "date-fns";
+
+/////////////
+// GET
+
+export async function getCountries() {
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v2/all?fields=name,flag"
+    );
+    const countries = await res.json();
+    return countries;
+  } catch {
+    throw new Error("Could not fetch countries");
+  }
+}
