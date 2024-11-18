e0c26d7bbffb9d8c6902a78f7e7b94a26f610f90
GhadeerAlbattarni <ghadeeralbattarni@gmail.com>
2024-11-18 14:02:00 -0500

    Streaming UI With Suspense: Cabin List

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,7 +1,38 @@
 import { eachDayOfInterval } from "date-fns";
-
+import { supabase } from "./supabase";
 /////////////
 // GET
+
+export const getCabins = async function () {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("id, name, maxCapacity, regularPrice, discount, image")
+    .order("name");
+
+  // For testing
+  // await new Promise((res) => setTimeout(res, 1000));
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+};
+
+export async function getCabin(id) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+}
 
 export async function getCountries() {
   try {
