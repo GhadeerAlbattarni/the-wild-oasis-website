98f66eccc840aac5c9e4e7aa11ecffbc7048d88e
GhadeerAlbattarni <ghadeeralbattarni@gmail.com>
2024-11-26 19:42:29 -0500

    Add update reservation feature

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -37,6 +38,46 @@ export async function updateGuestProfile(formData) {
   // this will revalidate the cash
   revalidatePath("/account/profile");
 }
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get("bookingId"));
+
+  // 1. Authentication
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  // 2. Authorization
+  // To make sure that user are trying to edit thier own bookings
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  // 3. Building update data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // 4. Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5. Error handling
+  if (error) throw new Error("Booking could not be updated");
+
+  // 6. Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+
+  // 7. Redireting
+  redirect("/account/reservations");
+}
+
 export async function deleteReservation(bookingId) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
